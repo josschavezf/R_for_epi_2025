05c3ddba27d3a21a864182de5ce3e6cd82e5c8ab
Joselyn Chavez <joselynchavezf@gmail.com>
2025-01-13 13:51:10 -0500

    add week2

---
+++ week_2/Assignment2.R
@@ -0,0 +1,38 @@
+### Introduction to Epidemiology Data Analysis with R.
+
+### Introduction to coding with R. Part I.
+
+# Assignment week 2:
+
+## 1) Which of the following options are good variable names?
+
+a) Patient$age
+b) cause_of_disease
+c) median
+d) DEMOGRAPHICINFORMATION
+e) incidenceRate
+
+
+## Let's create some vectors
+
+a <- 70
+b <- "hello"
+c <- TRUE
+
+### 2) What is the class of vector a?
+
+### 3) What is the class of vector c?
+
+## Store the result of the following code in a new vector
+d <- c(a,b,c)
+
+### 4) What is the resulting class of the new vector?
+
+## Let's create a vector that contains the weight of three patients:
+patients_weight <- c(198, 165, 150)
+
+### 5) How do you get the number of patients in the vector?
+
+### 6) How would you get the weight of patient 2?
+
+### 7) How would you get the weight of patients 2 and 3?
