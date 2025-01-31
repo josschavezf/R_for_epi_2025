faf8191b147767e061132a5a14d98441b7ab1d1e
Joselyn Chavez <joselynchavezf@gmail.com>
2025-01-31 15:03:45 -0500

    add week 5

---
+++ week_5/Assignment5.R
@@ -0,0 +1,28 @@
+### Introduction to Epidemiology Data Analysis with R. 
+
+### Data transformation
+
+# Assignment week 5: 
+
+
+## Read the cause_of_death.xlsx file and store the table within the cause_of_death variable
+cause_of_death <- readxl::read_excel("cause_of_death.xlsx")
+
+### 1) Order the table by year
+
+
+### 2) Create a new column deaths_k with the result of dividing deaths/1000
+
+
+### 3) From the previous result, select all columns except deaths 
+
+
+### 4) Select men who died of Cerebrovascular diseases in 2019. 
+### Which race had the lower number of deaths?
+
+
+### 5) What was the main cause of death among women Black or African-American 
+### population during 1980?
+
+
+
---
+++ week_5/group_exercise.R
@@ -0,0 +1,7 @@
+### 6) What was the mean number of deaths per year, race and sex? 
+### (Calculate all combinations at the same time, not separated)
+### The result should look like this:
+# 1980 American Indian or Alaska Native Female       0.205
+# 1980 American Indian or Alaska Native Male         0.332
+# 1980 Asian or Pacific Islander        Female       0.35 
+# 1980 Asian or Pacific Islander        Male         0.566
